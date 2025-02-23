db60a31e518fa36c2059d0bf39923101ba1b2e29
RamenFornow <almarttala@gmail.com>
2025-02-23 12:05:10 -0600

    Modifiers!
    Created Sleepy and Fly modifiers
    Updated car graphics

--- Assets/Scripts/CarGame/Car.cs
+++ Assets/Scripts/CarGame/Car.cs
@@ -1,13 +1,32 @@
+using System;
 using UnityEngine;
 
 public class Car : MonoBehaviour
 {
     public Rigidbody2D rb;
     public float speed;
+    public SpriteRenderer renderer;
+    public Sprite carSprite1;
+    public Sprite carSprite2;
+    public Sprite carSprite3;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb.linearVelocity = new Vector2(speed, 0f);
+        int spriteNum = UnityEngine.Random.Range(1, 3);
+        switch (spriteNum)
+        {
+            case 1:
+                renderer.sprite = carSprite1;
+                break;
+            case 2:
+                renderer.sprite = carSprite2;
+                break;
+            case 3:
+                renderer.sprite = carSprite3;
+                break;
+
+        }
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/ModifierHandler.cs
@@ -0,0 +1,113 @@
+using System.Runtime.InteropServices.ComTypes;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+public class ModifierHandler : MonoBehaviour
+{
+
+
+    private int day;
+    public VolumeProfile postprocessing;
+    public GameObject camera;
+    public float vignetteMaxValue;
+    public float vignetteMinValue;
+    private float vignetteCurrentValue = 0f;
+    private Vignette vignette;
+    public float vignetteModifier;
+    private ChromaticAberration chromab;
+    private LensDistortion lensdist;
+    private int sign = 1;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //get the modifiers from scenecontroller
+        switch (day)
+        {
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+            case 5:
+
+                break;
+            default:
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void FixedUpdate()
+    {
+        GuysOnScreen();
+    }
+    void SleepyScreen()
+    {
+        if (postprocessing.TryGet(out vignette))
+        {
+            vignetteCurrentValue = Oscillate(Time.time, vignetteModifier);
+            //vignette.intensity.value = vignetteCurrentValue;
+            vignette.intensity.SetValue((new UnityEngine.Rendering.FloatParameter(vignetteCurrentValue)));
+            print("VIGNETTE VALUE: " + (vignette.intensity.value));
+        }
+        if (postprocessing.TryGet(out chromab))
+        {
+            chromab.intensity.SetValue(new UnityEngine.Rendering.FloatParameter(vignetteCurrentValue / 2));
+        }
+        if (postprocessing.TryGet(out lensdist))
+        {
+            lensdist.intensity.SetValue(new UnityEngine.Rendering.FloatParameter(Mathf.Clamp(-(vignetteCurrentValue / 2), -0.6f, 0f)));
+        }
+    }
+    Vector2 flySpawn1 = new Vector2(12f, -7.5f);
+    Vector2 flySpawn2 = new Vector2(12f, 7.5f);
+    Vector2 flySpawn3 = new Vector2(-12f, -7.5f);
+    Vector2 flySpawn4 = new Vector2(-12f, 7.5f);
+    public GameObject flyPrefab;
+    public float flySpawnTime;
+    float flySpawnTimer = 0f;
+    void GuysOnScreen()
+    {
+        flySpawnTimer += 0.01f;
+        if (flySpawnTimer > flySpawnTime)
+        {
+            int SPnum = UnityEngine.Random.Range(1, 4);
+            Vector2 spawnPoint = new Vector2(-7.5f, -7.5f);
+            if (SPnum == 1) { spawnPoint = flySpawn1; }
+            else if (SPnum == 2) { spawnPoint = flySpawn2; }
+            else if (SPnum == 3) { spawnPoint = flySpawn3; }
+            else if (SPnum == 4) { spawnPoint = flySpawn4; }
+            Instantiate(flyPrefab, spawnPoint, Quaternion.identity);
+            flySpawnTimer = 0f;
+        }
+    }
+
+    void OnDestroy()
+    {
+        vignette.intensity.SetValue((new UnityEngine.Rendering.FloatParameter(0)));
+        chromab.intensity.SetValue(new UnityEngine.Rendering.FloatParameter(0));
+        lensdist.intensity.SetValue(new UnityEngine.Rendering.FloatParameter(0));
+    }
+    float Oscillate(float time, float speed)
+    {
+        float amplitude = (0.6f - (-0.1f)) / 2f; // Half the total range
+        float midpoint = (0.6f + (-0.1f)) / 2f;  // Midpoint of range
+
+        return midpoint + amplitude * Mathf.Sin(time * speed);
+    }
+
+}

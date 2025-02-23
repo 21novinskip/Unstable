432b7f653021419313b6a41b950a1e9c9a9f926a
PeterNovinski <peter.a.novinski@gmail.com>
2025-02-22 18:30:53 -0600

    Spreadsheet minigame base

--- Assets/Scripts/Spreadsheet Minigame/ButtonManager.cs
+++ Assets/Scripts/Spreadsheet Minigame/ButtonManager.cs
@@ -1,16 +1,105 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Collections.Generic;
 
 public class ButtonManager : MonoBehaviour
 {
+    private int Tasks_Completed;
+    public int CorrectButtons = 0;
+    public TextMeshProUGUI ScoreText;
+    public int Digits = 2;
+
+    public Button[] buttons; // Array to hold all 20 buttons
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Tasks_Completed = 0;
+        ScoreText.text = Tasks_Completed.ToString();
+        ResetButtons();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ResetButtons();
+        }
+
+        if (CorrectButtons == 2)
+        {
+            Win();
+        }
     }
+    void ResetButtons()
+    {
+        CorrectButtons = 0;
+
+        // Create a list to keep track of unique numbers
+        List<int> generatedNumbers = new List<int>();
+
+        // Generate unique numbers for each button
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            int randomNumber;
+            do
+            {
+                randomNumber = GenerateRandomNumber(Digits);
+            } 
+            while (generatedNumbers.Contains(randomNumber)); // Ensure uniqueness
+
+            generatedNumbers.Add(randomNumber);
+            buttons[i].GetComponentInChildren<TextMeshProUGUI>().text = randomNumber.ToString();
+        }
+
+        // Select two random buttons to be the "dupe" ones
+        int rando1 = Random.Range(0, buttons.Length);
+        int rando2;
+        do
+        {
+            rando2 = Random.Range(0, buttons.Length);
+        } while (rando2 == rando1);
+
+        Debug.Log("The special buttons are buttons " + rando1 + " and " + rando2);
+
+        // Generate a unique dupe number that is NOT in generatedNumbers
+        int dupeNumber;
+        do
+        {
+            dupeNumber = GenerateRandomNumber(Digits);
+        } while (generatedNumbers.Contains(dupeNumber)); // Ensure it's unique
+
+        // Assign the same number to both dupe buttons
+        buttons[rando1].GetComponentInChildren<TextMeshProUGUI>().text = dupeNumber.ToString();
+        buttons[rando2].GetComponentInChildren<TextMeshProUGUI>().text = dupeNumber.ToString();
+
+        // Mark these buttons as "dupe"
+        buttons[rando1].GetComponent<ButtonScript>().IsDupe = true;
+        buttons[rando2].GetComponent<ButtonScript>().IsDupe = true;
+    }
+
+    public void Win()
+    {
+        Debug.Log("You win this round!");
+        Tasks_Completed ++;
+        ScoreText.text = Tasks_Completed.ToString();
+        ResetButtons();
+    }
+    public void Lose()
+    {
+        Debug.Log("You Lost this round");
+        ResetButtons();
+    }
+
+    // Function to generate a random number with a specified number of digits
+    int GenerateRandomNumber(int digits)
+    {
+        int min = (int)Mathf.Pow(10, digits - 1); // Explicit cast to int
+        int max = (int)Mathf.Pow(10, digits) - 1;  // Explicit cast to int
+
+        return Random.Range(min, max + 1); // Generate random number in the specified range
+    }
+
 }
